f968a92cba3bbbd5e2f495cb183f7336f9ee866f
muhammadkevinpf <muhammadkevinpf@gmail.com>
2020-07-26 14:07:13 +0700

    about done

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "antd/dist/antd.css";
 import Navbar from "./components/Navbar";
 import Welcome from "./components/Welcome";
 import Feature from "./components/Feature";
+import About from "./components/About";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Welcome />
       <Feature />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import AboutImage from "../assets/images/illustration-stay-productive.png";
+import IconArrow from "../assets/images/icon-arrow.svg";
+import { Button } from "antd";
+function About() {
+  return (
+    <div className="about-section">
+      <img src={AboutImage} alt="stay productive" />
+      <div>
+        <h2>Stay productive, wherever you are</h2>
+        <p>
+          Never let location be an issue when accessing your files. Fylo has you
+          covered for all of your file storage needs.
+        </p>
+        <p>
+          Securely share files and folders with friends, family and colleagues
+          for live collaboration. No email attachments required.
+        </p>
+        <Button type="link">
+          See how Fylo works &nbsp;
+          <img src={IconArrow} alt="arrow" />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default About;
